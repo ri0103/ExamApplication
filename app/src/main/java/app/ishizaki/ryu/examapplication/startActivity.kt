961985273a202792fd2e172e32bb1caf25b5b7d4
Ryu Ishizaki <ryu2006ishizaki@gmail.com>
2021-12-22 18:23:34 +0900

    show bottom navigation done

--- app/src/main/java/app/ishizaki/ryu/examapplication/startActivity.kt
+++ app/src/main/java/app/ishizaki/ryu/examapplication/startActivity.kt
@@ -1,31 +1,54 @@
 package app.ishizaki.ryu.examapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import app.ishizaki.ryu.examapplication.fragments.CoverageFragment
+import app.ishizaki.ryu.examapplication.fragments.HomeFragment
+import app.ishizaki.ryu.examapplication.fragments.TodoFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_start.*
 
 class startActivity : AppCompatActivity() {
+
+
+
+    private val todoFragment = TodoFragment()
+    private val homeFragment = HomeFragment()
+    private val coverageFragment = CoverageFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        replaceFragment(homeFragment)
 
 
-
-            moveToDoButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
-        moveCoverageButton.setOnClickListener {
-            val intent = Intent(this, CoverageMainActivity::class.java)
-            startActivity(intent)
-        }
-
-
+bottom_navigation.setOnNavigationItemSelectedListener {
+    when(it.itemId){
+        R.id.ic_todo -> replaceFragment(todoFragment)
+        R.id.ic_home -> replaceFragment(homeFragment)
+        R.id.ic_coverage -> replaceFragment(coverageFragment)
+    }
+    true
+}
 
 
 
     }
+
+
+
+
+    private fun replaceFragment (fragment: Fragment){
+        if (fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+    }
+
+
+
+
 }
